017dcbcf7b8e350ea89a372d3048a735142c3b32
sonson <yoshida.yuichi@gmail.com>
2017-04-23 13:40:53 +0900

    Added code to receive callback.

--- swiftodon/AppDelegate.swift
+++ swiftodon/AppDelegate.swift
@@ -13,6 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print(#function)
+        print(url)
+        print(options)
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
